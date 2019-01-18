1961724835ece63a5c5e672e50d6f7ea82b8e953
Antonio Juliano <antonio.m.juliano@gmail.com>
2019-01-17 17:17:35 -0800

    Publish to npm

--- src/lib/Artifacts.ts
+++ src/lib/Artifacts.ts
@@ -1,8 +1,19 @@
-export { default as SoloMargin } from '../../build/contracts/SoloMargin.json';
-export { default as IErc20 } from '../../build/contracts/IErc20.json';
-export { default as Expiry } from '../../build/contracts/Expiry.json';
-export { default as TokenA } from '../../build/contracts/TokenA.json';
-export { default as TokenB } from '../../build/contracts/TokenB.json';
-export { default as TokenC } from '../../build/contracts/TokenC.json';
-export { default as TestPriceOracle } from '../../build/contracts/TestPriceOracle.json';
-export { default as TestInterestSetter } from '../../build/contracts/TestInterestSetter.json';
+import { default as SoloMargin } from '../../build/contracts/SoloMargin.json';
+import { default as IErc20 } from '../../build/contracts/IErc20.json';
+import { default as Expiry } from '../../build/contracts/Expiry.json';
+import { default as TokenA } from '../../build/contracts/TokenA.json';
+import { default as TokenB } from '../../build/contracts/TokenB.json';
+import { default as TokenC } from '../../build/contracts/TokenC.json';
+import { default as TestPriceOracle } from '../../build/contracts/TestPriceOracle.json';
+import { default as TestInterestSetter } from '../../build/contracts/TestInterestSetter.json';
+
+export default {
+  SoloMargin,
+  IErc20,
+  Expiry,
+  TokenA,
+  TokenB,
+  TokenC,
+  TestPriceOracle,
+  TestInterestSetter,
+};
