b276fb65a7d231bc226c5141849424570178064d
Brendan Chou <brendanchou@msn.com>
2019-02-12 13:22:20 -0800

    add Sell and Buy tests

--- src/modules/testing/TestExchangeWrapper.ts
+++ src/modules/testing/TestExchangeWrapper.ts
@@ -12,4 +12,8 @@ export class TestExchangeWrapper {
   public getAddress(): string {
     return this.contracts.testExchangeWrapper.options.address;
   }
+
+  public getExchangeAddress(): string {
+    return '0x0000000000000000000000000000000000000001';
+  }
 }
