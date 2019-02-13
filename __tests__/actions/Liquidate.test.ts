fabf4366916db79d399cdeb80ae000099e6eb8ee
Brendan Chou <brendanchou@msn.com>
2019-02-12 17:25:50 -0800

    add more liquidate and vaporize tests

--- __tests__/actions/Liquidate.test.ts
+++ __tests__/actions/Liquidate.test.ts
@@ -1,106 +1,336 @@
 import BigNumber from 'bignumber.js';
 import { getSolo } from '../helpers/Solo';
 import { Solo } from '../../src/Solo';
-import { address, AmountDenomination, AmountReference } from '../../src/types';
-import { mineAvgBlock, resetEVM } from '../helpers/EVM';
+import { resetEVM, snapshot } from '../helpers/EVM';
 import { setupMarkets } from '../helpers/SoloHelpers';
 import { INTEGERS } from '../../src/lib/Constants';
+import { expectThrow } from '../../src/lib/Expect';
+import {
+  AccountStatus,
+  address,
+  AmountDenomination,
+  AmountReference,
+  Liquidate,
+  Integer,
+} from '../../src/types';
+
+let liquidOwner: address;
+let solidOwner: address;
+let operator: address;
+let solo: Solo;
+let accounts: address[];
+const liquidAccountNumber = INTEGERS.ZERO;
+const solidAccountNumber = INTEGERS.ONE;
+const owedMarket = INTEGERS.ZERO;
+const heldMarket = INTEGERS.ONE;
+const otherMarket = new BigNumber(2);
+const zero = new BigNumber(0);
+const par = new BigNumber(10000);
+const wei = new BigNumber(15000);
+const negPar = new BigNumber(-10000);
+const collateralization = new BigNumber('1.1');
+const collatPar = par.times(collateralization);
+const premium = new BigNumber('1.05');
+const remaining = collateralization.minus(premium);
+let defaultGlob: Liquidate;
 
 describe('Liquidate', () => {
-  let solo: Solo;
-  let accounts: address[];
+  let snapshotId: string;
 
   beforeAll(async () => {
     const r = await getSolo();
     solo = r.solo;
     accounts = r.accounts;
+    solidOwner = solo.getDefaultAccount();
+    liquidOwner = accounts[6];
+    operator = accounts[7];
+    defaultGlob = {
+      primaryAccountOwner: solidOwner,
+      primaryAccountId: solidAccountNumber,
+      liquidAccountOwner: liquidOwner,
+      liquidAccountId: liquidAccountNumber,
+      liquidMarketId: owedMarket,
+      payoutMarketId: heldMarket,
+      amount: {
+        value: zero,
+        denomination: AmountDenomination.Principal,
+        reference: AmountReference.Target,
+      },
+    };
+
+    await resetEVM();
+    await setupMarkets(solo, accounts);
+    const defaultIndex = {
+      lastUpdate: INTEGERS.ZERO,
+      borrow: wei.div(par),
+      supply: wei.div(par),
+    };
+    await Promise.all([
+      solo.testing.setMarketIndex(owedMarket, defaultIndex),
+      solo.testing.setMarketIndex(heldMarket, defaultIndex),
+      solo.testing.setAccountBalance(liquidOwner, liquidAccountNumber, owedMarket, negPar),
+      solo.testing.setAccountBalance(liquidOwner, liquidAccountNumber, heldMarket, collatPar),
+      solo.testing.setAccountBalance(solidOwner, solidAccountNumber, owedMarket, par),
+    ]);
+    snapshotId = await snapshot();
   });
 
   beforeEach(async () => {
-    await resetEVM();
-    await mineAvgBlock();
+    await resetEVM(snapshotId);
   });
 
   it('Basic liquidate test', async () => {
-    await setupMarkets(solo, accounts);
+    const txResult = await expectLiquidateOkay({});
 
-    const fullAmount = new BigNumber(100);
-    const who1 = solo.getDefaultAccount();
-    const who2 = accounts[2];
-    expect(who1).not.toEqual(who2);
-    const accountNumber1 = INTEGERS.ZERO;
-    const accountNumber2 = INTEGERS.ONE;
-    const marketA = INTEGERS.ZERO;
-    const marketB = INTEGERS.ONE;
-    const collateralization = new BigNumber('1.1');
-    const premium = new BigNumber('1.05');
+    console.log(`\tLiquidate gas used: ${txResult.gasUsed}`);
 
     await Promise.all([
-      // set balances
-      solo.testing.setAccountBalance(
-        who1,
-        accountNumber1,
-        marketB,
-        fullAmount.times(5),
-      ),
-      solo.testing.setAccountBalance(
-        who2,
-        accountNumber2,
-        marketA,
-        fullAmount.times(collateralization),
-      ),
-      solo.testing.setAccountBalance(
-        who2,
-        accountNumber2,
-        marketB,
-        fullAmount.times(-1),
-      ),
+      expectSolidPars(par.times(premium), zero),
+      expectLiquidPars(par.times(remaining), zero),
     ]);
 
-    const { gasUsed } = await solo.operation.initiate()
-      .liquidate({
-        primaryAccountOwner: who1,
-        primaryAccountId: accountNumber1,
-        liquidAccountOwner: who2,
-        liquidAccountId: accountNumber2,
-        liquidMarketId: marketB,
-        payoutMarketId: marketA,
+    // TODO: check log
+  });
+
+  it('Succeeds when partially liquidating', async () => {
+    await expectLiquidateOkay({
+      amount: {
+        value: par.times(2),
+        denomination: AmountDenomination.Principal,
+        reference: AmountReference.Delta,
+      },
+    });
+
+    await Promise.all([
+      expectSolidPars(par.times(premium), zero),
+      expectLiquidPars(par.times(remaining), zero),
+    ]);
+  });
+
+  it('Succeeds when bound by owedToken', async () => {
+    await expectLiquidateOkay({
+      amount: {
+        value: par.times(2),
+        denomination: AmountDenomination.Principal,
+        reference: AmountReference.Delta,
+      },
+    });
+
+    await Promise.all([
+      expectSolidPars(par.times(premium), zero),
+      expectLiquidPars(par.times(remaining), zero),
+    ]);
+  });
+
+  it('Succeeds when bound by heldToken', async () => {
+    const amount = par.times(premium).div(2);
+    await solo.testing.setAccountBalance(liquidOwner, liquidAccountNumber, heldMarket, amount);
+    await expectLiquidateOkay({});
+
+    await Promise.all([
+      expectSolidPars(par.times(premium).div(2), par.div(2)),
+      expectLiquidPars(zero, negPar.div(2)),
+    ]);
+  });
+
+  it('Succeeds for solid account that takes on a negative balance', async () => {
+    await solo.testing.setAccountBalance(solidOwner, solidAccountNumber, owedMarket, par.div(2));
+    await expectLiquidateOkay({});
+    await Promise.all([
+      expectSolidPars(par.times(premium), negPar.div(2)),
+      expectLiquidPars(par.times(remaining), zero),
+    ]);
+  });
+
+  it('Succeeds for account already marked with liquidating flag', async () => {
+    const amount = par.times(2);
+    await Promise.all([
+      solo.testing.setAccountBalance(liquidOwner, liquidAccountNumber, heldMarket, amount),
+      solo.testing.setAccountStatus(liquidOwner, liquidAccountNumber, AccountStatus.Liquidating),
+    ]);
+    await expectLiquidateOkay({});
+    await Promise.all([
+      expectSolidPars(par.times(premium), zero),
+      expectLiquidPars(amount.minus(par.times(premium)), zero),
+    ]);
+  });
+
+  it('Succeeds for operator', async () => {
+    await solo.permissions.approveOperator(operator, { from: solidOwner });
+    await expectLiquidateOkay({}, { from: operator });
+
+    await Promise.all([
+      expectSolidPars(par.times(premium), zero),
+      expectLiquidPars(par.times(remaining), zero),
+    ]);
+  });
+
+  it('Succeeds (without effect) for zero collateral', async () => {
+    await expectLiquidateOkay({
+      payoutMarketId: otherMarket,
+    });
+    await Promise.all([
+      expectSolidPars(zero, par),
+      expectLiquidPars(collatPar, negPar),
+    ]);
+    const totalOtherPar = await solo.getters.getMarket(otherMarket);
+    expect(totalOtherPar.market.totalPar.supply).toEqual(zero);
+    expect(totalOtherPar.market.totalPar.borrow).toEqual(zero);
+  });
+
+  it('Succeeds (without effect) for zero loan', async () => {
+    await expectLiquidateOkay({
+      liquidMarketId: otherMarket,
+    });
+    await Promise.all([
+      expectSolidPars(zero, par),
+      expectLiquidPars(collatPar, negPar),
+    ]);
+    const totalOtherPar = await solo.getters.getMarket(otherMarket);
+    expect(totalOtherPar.market.totalPar.supply).toEqual(zero);
+    expect(totalOtherPar.market.totalPar.borrow).toEqual(zero);
+  });
+
+  it('Fails for over-collateralized account', async () => {
+    const amount = par.times(2);
+    await solo.testing.setAccountBalance(liquidOwner, liquidAccountNumber, heldMarket, amount);
+    await expectLiquidateRevert({}, 'OperationImpl: Unliquidatable account');
+  });
+
+  it('Fails for non-operator', async () => {
+    await expectLiquidateRevert(
+      {},
+      'Storage: Unpermissioned Operator',
+      { from: operator },
+    );
+  });
+
+  it('Fails if liquidating after account used as primary', async () => {
+    await solo.permissions.approveOperator(solidOwner, { from: liquidOwner });
+    const operation = solo.operation.initiate();
+    operation.deposit({
+      primaryAccountOwner: liquidOwner,
+      primaryAccountId: liquidAccountNumber,
+      marketId: owedMarket,
+      from: liquidOwner,
+      amount: {
+        value: par.div(2),
+        denomination: AmountDenomination.Principal,
+        reference: AmountReference.Delta,
+      },
+    });
+    operation.liquidate(defaultGlob);
+    await expectThrow(
+      operation.commit(),
+      'OperationImpl: Requires non-primary account',
+    );
+  });
+
+  it('Fails if liquidating totally zero account', async () => {
+    await expectLiquidateRevert(
+      {
+        liquidAccountOwner: liquidOwner,
+        liquidAccountId: solidAccountNumber,
+      },
+      'OperationImpl: Unliquidatable account',
+    );
+  });
+
+  it('Fails for repeated market', async () => {
+    await expectLiquidateRevert(
+      { payoutMarketId: owedMarket },
+      'OperationImpl: Markets must be distinct',
+    );
+  });
+
+  it('Fails for negative collateral', async () => {
+    await solo.testing.setAccountBalance(liquidOwner, liquidAccountNumber, heldMarket, negPar);
+    await expectLiquidateRevert(
+      {},
+      'OperationImpl: Collateral cannot be negative',
+    );
+  });
+
+  it('Fails for paying back market that is already positive', async () => {
+    await Promise.all([
+      solo.testing.setAccountBalance(liquidOwner, liquidAccountNumber, owedMarket, collatPar),
+      solo.testing.setAccountBalance(liquidOwner, liquidAccountNumber, heldMarket, negPar),
+    ]);
+    await expectLiquidateRevert(
+      {
+        payoutMarketId: otherMarket,
+      },
+      'Storage: Owed balance cannot be positive',
+    );
+  });
+
+  it('Fails for a negative delta', async () => {
+    await expectLiquidateRevert(
+      {
         amount: {
-          value: INTEGERS.ZERO,
-          denomination: AmountDenomination.Actual,
+          value: negPar.times(2),
+          denomination: AmountDenomination.Principal,
           reference: AmountReference.Target,
         },
-      })
-      .commit();
-
-    console.log(`\tLiquidate gas used: ${gasUsed}`);
-
-    const [
-      balancesA,
-      balancesB,
-    ] = await Promise.all([
-      solo.getters.getAccountBalances(who1, accountNumber1),
-      solo.getters.getAccountBalances(who2, accountNumber2),
-    ]);
-
-    balancesA.forEach((balance, i) => {
-      let expected = INTEGERS.ZERO;
-      if (i === marketA.toNumber()) {
-        expected = fullAmount.times(premium);
-      }
-      if (i === marketB.toNumber()) {
-        expected = fullAmount.times(4);
-      }
-      expect(balance.par).toEqual(expected);
-      expect(balance.wei).toEqual(expected);
-    });
-    balancesB.forEach((balance, i) => {
-      let expected = INTEGERS.ZERO;
-      if (i === marketA.toNumber()) {
-        expected = fullAmount.times(collateralization.minus(premium));
-      }
-      expect(balance.par).toEqual(expected);
-      expect(balance.wei).toEqual(expected);
-    });
+      },
+      'Storage: Owed balance cannot increase',
+    );
   });
 });
+
+// ============ Helper Functions ============
+
+async function expectLiquidationFlagSet() {
+  const status = await solo.getters.getAccountStatus(liquidOwner, liquidAccountNumber);
+  expect(status).toEqual(AccountStatus.Liquidating);
+}
+
+async function expectLiquidateOkay(
+  glob: Object,
+  options?: Object,
+) {
+  const combinedGlob = { ...defaultGlob, ...glob };
+  const txResult = await solo.operation.initiate().liquidate(combinedGlob).commit(options);
+  await expectLiquidationFlagSet();
+  return txResult;
+}
+
+async function expectLiquidateRevert(
+  glob: Object,
+  reason?: string,
+  options?: Object,
+) {
+  await expectThrow(expectLiquidateOkay(glob, options), reason);
+}
+
+async function expectSolidPars(
+  expectedHeldPar: Integer,
+  expectedOwedPar: Integer,
+) {
+  const balances = await solo.getters.getAccountBalances(solidOwner, solidAccountNumber);
+  balances.forEach((balance, i) => {
+    if (i === heldMarket.toNumber()) {
+      expect(balance.par).toEqual(expectedHeldPar);
+    } else if (i === owedMarket.toNumber()) {
+      expect(balance.par).toEqual(expectedOwedPar);
+    } else {
+      expect(balance.par).toEqual(zero);
+    }
+  });
+}
+
+async function expectLiquidPars(
+  expectedHeldPar: Integer,
+  expectedOwedPar: Integer,
+) {
+  const balances = await solo.getters.getAccountBalances(liquidOwner, liquidAccountNumber);
+  balances.forEach((balance, i) => {
+    if (i === heldMarket.toNumber()) {
+      expect(balance.par).toEqual(expectedHeldPar);
+    } else if (i === owedMarket.toNumber()) {
+      expect(balance.par).toEqual(expectedOwedPar);
+    } else {
+      expect(balance.par).toEqual(zero);
+    }
+  });
+}
--- __tests__/actions/Vaporize.test.ts
+++ __tests__/actions/Vaporize.test.ts
@@ -1,79 +1,391 @@
 import BigNumber from 'bignumber.js';
 import { getSolo } from '../helpers/Solo';
 import { Solo } from '../../src/Solo';
-import { address, AmountDenomination, AmountReference } from '../../src/types';
-import { mineAvgBlock, resetEVM } from '../helpers/EVM';
+import { resetEVM, snapshot } from '../helpers/EVM';
 import { setupMarkets } from '../helpers/SoloHelpers';
 import { INTEGERS } from '../../src/lib/Constants';
+import { expectThrow } from '../../src/lib/Expect';
+import {
+  AccountStatus,
+  address,
+  AmountDenomination,
+  AmountReference,
+  Vaporize,
+  Integer,
+} from '../../src/types';
+
+let vaporOwner: address;
+let solidOwner: address;
+let operator: address;
+let solo: Solo;
+let accounts: address[];
+const vaporAccountNumber = INTEGERS.ZERO;
+const solidAccountNumber = INTEGERS.ONE;
+const owedMarket = INTEGERS.ZERO;
+const heldMarket = INTEGERS.ONE;
+const otherMarket = new BigNumber(2);
+const zero = new BigNumber(0);
+const par = new BigNumber(10000);
+const wei = new BigNumber(15000);
+const negPar = par.times(-1);
+const negWei = wei.times(-1);
+const premium = new BigNumber('1.05');
+let defaultGlob: Vaporize;
 
 describe('Vaporize', () => {
-  let solo: Solo;
-  let accounts: address[];
+  let snapshotId: string;
 
   beforeAll(async () => {
     const r = await getSolo();
     solo = r.solo;
     accounts = r.accounts;
+    solidOwner = solo.getDefaultAccount();
+    vaporOwner = accounts[6];
+    operator = accounts[7];
+    defaultGlob = {
+      primaryAccountOwner: solidOwner,
+      primaryAccountId: solidAccountNumber,
+      vaporAccountOwner: vaporOwner,
+      vaporAccountId: vaporAccountNumber,
+      vaporMarketId: owedMarket,
+      payoutMarketId: heldMarket,
+      amount: {
+        value: zero,
+        denomination: AmountDenomination.Principal,
+        reference: AmountReference.Target,
+      },
+    };
+
+    await resetEVM();
+    await setupMarkets(solo, accounts);
+    const defaultIndex = {
+      lastUpdate: INTEGERS.ZERO,
+      borrow: wei.div(par),
+      supply: wei.div(par),
+    };
+    await Promise.all([
+      solo.testing.setMarketIndex(owedMarket, defaultIndex),
+      solo.testing.setMarketIndex(heldMarket, defaultIndex),
+      solo.testing.setAccountBalance(vaporOwner, vaporAccountNumber, owedMarket, negPar),
+      solo.testing.setAccountBalance(solidOwner, solidAccountNumber, owedMarket, par),
+    ]);
+    snapshotId = await snapshot();
   });
 
   beforeEach(async () => {
-    await resetEVM();
-    await mineAvgBlock();
+    await resetEVM(snapshotId);
   });
 
   it('Basic vaporize test', async () => {
-    await setupMarkets(solo, accounts);
+    const payoutAmount = wei.times(premium);
+    await issueHeldTokensToSolo(payoutAmount);
+    await expectExcessHeldToken(payoutAmount);
 
-    const fullAmount = new BigNumber(100);
-    const who1 = solo.getDefaultAccount();
-    const who2 = accounts[2];
-    expect(who1).not.toEqual(who2);
-    const accountNumber1 = INTEGERS.ZERO;
-    const accountNumber2 = INTEGERS.ONE;
-    const marketA = INTEGERS.ZERO;
-    const marketB = INTEGERS.ONE;
+    const txResult = await expectVaporizeOkay({});
+
+    console.log(`\tVaporize gas used: ${txResult.gasUsed}`);
 
     await Promise.all([
-      // set balances
-      solo.testing.setAccountBalance(
-        who1,
-        accountNumber1,
-        marketB,
-        fullAmount,
-      ),
-      solo.testing.setAccountBalance(
-        who2,
-        accountNumber2,
-        marketB,
-        fullAmount.times(-1),
-      ),
-
-      // set tokens
-      solo.testing.tokenA.issueTo(
-        fullAmount.times(2),
-        solo.contracts.soloMargin.options.address,
-      ),
+      expectExcessHeldToken(zero),
+      expectVaporPars(zero, zero),
+      expectSolidPars(par.times(premium), zero),
     ]);
 
-    const { gasUsed } = await solo.operation.initiate()
-      .vaporize({
-        primaryAccountOwner: who1,
-        primaryAccountId: accountNumber1,
-        vaporAccountOwner: who2,
-        vaporAccountId: accountNumber2,
-        vaporMarketId: marketB,
-        payoutMarketId: marketA,
+    // TODO: expect log
+  });
+
+  it('Fails for unvaporizable account', async () => {
+    await solo.testing.setAccountBalance(vaporOwner, vaporAccountNumber, heldMarket, par);
+    await expectVaporizeRevert({}, 'OperationImpl: Unvaporizable account');
+  });
+
+  it('Succeeds if enough excess owedTokens', async () => {
+    await issueOwedTokensToSolo(wei);
+    await expectExcessOwedToken(wei);
+
+    await expectVaporizeOkay({});
+
+    await Promise.all([
+      expectExcessOwedToken(zero),
+      expectVaporPars(zero, zero),
+      expectSolidPars(zero, par),
+    ]);
+
+    // TODO: expect log
+  });
+
+  it('Succeeds if half excess owedTokens', async () => {
+    const payoutAmount = wei.times(premium);
+    await Promise.all([
+      issueHeldTokensToSolo(payoutAmount),
+      issueOwedTokensToSolo(wei.div(2)),
+    ]);
+
+    await expectVaporizeOkay({});
+
+    await Promise.all([
+      expectExcessHeldToken(payoutAmount.div(2)),
+      expectExcessOwedToken(zero),
+      expectVaporPars(zero, zero),
+      expectSolidPars(par.times(premium).div(2), par.div(2)),
+    ]);
+
+    // TODO: expect log
+  });
+
+  it('Succeeds when bound by owedToken', async () => {
+    const payoutAmount = wei.times(premium);
+    await issueHeldTokensToSolo(payoutAmount.times(2));
+
+    await expectVaporizeOkay({
+      amount: {
+        value: par.times(2),
+        denomination: AmountDenomination.Principal,
+        reference: AmountReference.Delta,
+      },
+    });
+
+    await Promise.all([
+      expectExcessHeldToken(payoutAmount),
+      expectVaporPars(zero, zero),
+      expectSolidPars(par.times(premium), zero),
+    ]);
+  });
+
+  it('Succeeds when bound by heldToken', async () => {
+    const payoutAmount = wei.times(premium).div(2);
+    await issueHeldTokensToSolo(payoutAmount);
+
+    await expectVaporizeOkay({});
+
+    await Promise.all([
+      expectExcessHeldToken(zero),
+      expectVaporPars(zero, negPar.div(2)),
+      expectSolidPars(par.times(premium).div(2), par.div(2)),
+    ]);
+  });
+
+  it('Succeeds for account already marked with liquidating flag', async () => {
+    const payoutAmount = wei.times(premium);
+    await Promise.all([
+      issueHeldTokensToSolo(payoutAmount),
+      solo.testing.setAccountStatus(vaporOwner, vaporAccountNumber, AccountStatus.Liquidating),
+    ]);
+
+    await expectVaporizeOkay({});
+
+    await Promise.all([
+      expectExcessHeldToken(zero),
+      expectVaporPars(zero, zero),
+      expectSolidPars(par.times(premium), zero),
+    ]);
+  });
+
+  it('Succeeds for account already marked with vaporizing flag', async () => {
+    const payoutAmount = wei.times(premium);
+    await Promise.all([
+      issueHeldTokensToSolo(payoutAmount),
+      solo.testing.setAccountStatus(vaporOwner, vaporAccountNumber, AccountStatus.Vaporizing),
+    ]);
+
+    await expectVaporizeOkay({});
+
+    await Promise.all([
+      expectExcessHeldToken(zero),
+      expectVaporPars(zero, zero),
+      expectSolidPars(par.times(premium), zero),
+    ]);
+  });
+
+  it('Succeeds for solid account that takes on a negative balance', async () => {
+    const payoutAmount = wei.times(premium);
+    await Promise.all([
+      issueHeldTokensToSolo(payoutAmount),
+      solo.testing.setAccountBalance(solidOwner, solidAccountNumber, owedMarket, par.div(2)),
+      // need another positive balance so there is zero (or negative) excess owedToken
+      solo.testing.setAccountBalance(operator, solidAccountNumber, owedMarket, par),
+    ]);
+    await expectVaporizeOkay({});
+    await Promise.all([
+      expectExcessHeldToken(zero),
+      expectVaporPars(zero, zero),
+      expectSolidPars(par.times(premium), negPar.div(2)),
+    ]);
+  });
+
+  it('Succeeds for operator', async () => {
+    const payoutAmount = wei.times(premium);
+    await Promise.all([
+      issueHeldTokensToSolo(payoutAmount),
+      solo.permissions.approveOperator(operator, { from: solidOwner }),
+    ]);
+    await expectVaporizeOkay({}, { from: operator });
+    await Promise.all([
+      expectExcessHeldToken(zero),
+      expectVaporPars(zero, zero),
+      expectSolidPars(par.times(premium), zero),
+    ]);
+  });
+
+  it('Succeeds (without effect) for zero excess', async () => {
+    await expectExcessHeldToken(zero);
+    await expectVaporizeOkay({});
+    await Promise.all([
+      expectExcessHeldToken(zero),
+      expectVaporPars(zero, negPar),
+      expectSolidPars(zero, par),
+    ]);
+  });
+
+  it('Succeeds (without effect) for zero loan', async () => {
+    const payoutAmount = wei.times(premium);
+    await issueHeldTokensToSolo(payoutAmount);
+    await expectVaporizeOkay({
+      vaporMarketId: otherMarket,
+    });
+    await Promise.all([
+      expectExcessHeldToken(payoutAmount),
+      expectVaporPars(zero, negPar),
+      expectSolidPars(zero, par),
+    ]);
+  });
+
+  it('Fails for non-operator', async () => {
+    const payoutAmount = wei.times(premium);
+    await issueHeldTokensToSolo(payoutAmount);
+    await expectVaporizeRevert(
+      {},
+      'Storage: Unpermissioned Operator',
+      { from: operator },
+    );
+  });
+
+  it('Fails if vaporizing after account used as primary', async () => {
+    await solo.permissions.approveOperator(solidOwner, { from: vaporOwner });
+    const operation = solo.operation.initiate();
+    operation.deposit({
+      primaryAccountOwner: vaporOwner,
+      primaryAccountId: vaporAccountNumber,
+      marketId: owedMarket,
+      from: vaporOwner,
+      amount: {
+        value: par.div(2),
+        denomination: AmountDenomination.Principal,
+        reference: AmountReference.Delta,
+      },
+    });
+    operation.vaporize(defaultGlob);
+    await expectThrow(
+      operation.commit(),
+      'OperationImpl: Requires non-primary account',
+    );
+  });
+
+  it('Fails if vaporizing totally zero account', async () => {
+    await expectVaporizeRevert(
+      {
+        vaporAccountOwner: operator,
+        vaporAccountId: zero,
+      },
+      'OperationImpl: Unvaporizable account',
+    );
+  });
+
+  it('Fails for repeated market', async () => {
+    await expectVaporizeRevert(
+      { payoutMarketId: owedMarket },
+      'OperationImpl: Markets must be distinct',
+    );
+  });
+
+  it('Fails for negative excess heldTokens', async () => {
+    await solo.testing.setAccountBalance(solidOwner, solidAccountNumber, heldMarket, par);
+    await expectExcessHeldToken(negWei);
+    await expectVaporizeRevert({}, 'OperationImpl: Excess cannot be negative');
+  });
+
+  it('Fails for a negative delta', async () => {
+    await expectVaporizeRevert(
+      {
         amount: {
-          value: INTEGERS.ZERO,
-          denomination: AmountDenomination.Actual,
+          value: negPar.times(2),
+          denomination: AmountDenomination.Principal,
           reference: AmountReference.Target,
         },
-      })
-      .commit();
-
-    console.log(`\tVaporize gas used: ${gasUsed}`);
-
-    // TODO: check excess token amounts before and after
-    // TODO: check balances before and after
+      },
+      'Storage: Owed balance cannot increase',
+    );
   });
 });
+
+// ============ Helper Functions ============
+
+async function issueHeldTokensToSolo(amount: BigNumber) {
+  return await solo.testing.tokenB.issueTo(amount, solo.contracts.soloMargin.options.address);
+}
+
+async function issueOwedTokensToSolo(amount: BigNumber) {
+  return await solo.testing.tokenA.issueTo(amount, solo.contracts.soloMargin.options.address);
+}
+
+async function expectVaporizeOkay(
+  glob: Object,
+  options?: Object,
+) {
+  const combinedGlob = { ...defaultGlob, ...glob };
+  return await solo.operation.initiate().vaporize(combinedGlob).commit(options);
+}
+
+async function expectVaporizeRevert(
+  glob: Object,
+  reason?: string,
+  options?: Object,
+) {
+  await expectThrow(expectVaporizeOkay(glob, options), reason);
+}
+
+async function expectSolidPars(
+  expectedHeldPar: Integer,
+  expectedOwedPar: Integer,
+) {
+  const balances = await solo.getters.getAccountBalances(solidOwner, solidAccountNumber);
+  balances.forEach((balance, i) => {
+    if (i === heldMarket.toNumber()) {
+      expect(balance.par).toEqual(expectedHeldPar);
+    } else if (i === owedMarket.toNumber()) {
+      expect(balance.par).toEqual(expectedOwedPar);
+    } else {
+      expect(balance.par).toEqual(zero);
+    }
+  });
+}
+
+async function expectVaporPars(
+  expectedHeldPar: Integer,
+  expectedOwedPar: Integer,
+) {
+  const balances = await solo.getters.getAccountBalances(vaporOwner, vaporAccountNumber);
+  balances.forEach((balance, i) => {
+    if (i === heldMarket.toNumber()) {
+      expect(balance.par).toEqual(expectedHeldPar);
+    } else if (i === owedMarket.toNumber()) {
+      expect(balance.par).toEqual(expectedOwedPar);
+    } else {
+      expect(balance.par).toEqual(zero);
+    }
+  });
+}
+
+async function expectExcessHeldToken(
+  expected: Integer,
+) {
+  const actual = await solo.getters.getNumExcessTokens(heldMarket);
+  expect(actual).toEqual(expected);
+}
+
+async function expectExcessOwedToken(
+  expected: Integer,
+) {
+  const actual = await solo.getters.getNumExcessTokens(owedMarket);
+  expect(actual).toEqual(expected);
+}
