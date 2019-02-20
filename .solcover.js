20e46ff3d9348f4299967253b748f439a8c550ea
Brendan Chou <brendanchou@msn.com>
2019-02-20 12:22:08 -0800

    don't cover external contracts

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,7 @@
 module.exports = {
   compileCommand: 'npm run build',
   copyPackages: ['openzeppelin-solidity', '@dydxprotocol/exchange-wrappers'],
-  skipFiles: ['testing/', 'Migrations.sol'],
+  skipFiles: ['testing/', 'Migrations.sol', 'external/'],
   testCommand: 'npm run test_cov',
   testrpcOptions: '-d --port 8555 -i 1002',
 };
