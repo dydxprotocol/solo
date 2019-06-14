ea05d41ff3c7d36c7254bffeb8be0b2ec443167b
Brendan Chou <brendanchou@msn.com>
2019-06-13 17:29:14 -0700

    Update interest rate functions (#270)
    * update interest rate functions
    
    * 0.8.0
    
    * fix

--- migrations/helpers.js
+++ migrations/helpers.js
@@ -53,7 +53,13 @@ async function getRiskParams(network) {
   };
 }
 
-async function getPolynomialParams() {
+async function getPolynomialParams(network) {
+  if (isMainNet(network)) {
+    return {
+      maxAPR: decimalToString('0.75'), // 75%
+      coefficients: coefficientsToString([0, 15, 0, 0, 0, 0, 85]),
+    };
+  }
   return {
     maxAPR: decimalToString('1.00'), // 100%
     coefficients: coefficientsToString([0, 10, 10, 0, 0, 80]),
